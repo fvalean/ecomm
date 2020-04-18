37cc4f6687f615345003d76985e2f026f43f4c79
Florin Valean <36827144+fvalean@users.noreply.github.com>
2020-04-18 02:20:33 -0400

    Added CORS

--- API/Startup.cs
+++ API/Startup.cs
@@ -28,6 +28,13 @@ namespace API
             //order dependent
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +45,7 @@ namespace API
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseStaticFiles();
+            app.UseCors("CorsPolicy");
             app.UseAuthorization();
             app.UseSwagger();
             app.UseSwaggerDocumentation();
