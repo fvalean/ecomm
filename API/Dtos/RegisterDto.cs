cd8c8440ff6400662634354defe2bb5893d0c626
Florin Valean <36827144+fvalean@users.noreply.github.com>
2020-05-02 02:17:19 -0400

    Api validation

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,9 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos
 {
     public class RegisterDto
     {
+        [Required]
         public string DisplayName { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
+        [RegularExpression("(?=^.{6,20}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
+        ErrorMessage = "Password must contain an uppercase character, lowercase character, a digit, and a non-alphanumeric character. Passwords must be between 6 and 20 characters long")]
         public string Password { get; set; }
     }
 }
