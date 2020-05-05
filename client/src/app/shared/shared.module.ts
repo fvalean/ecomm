27c2ed035c87706caa87c1db8526ebefce620a0b
Florin Valean <36827144+fvalean@users.noreply.github.com>
2020-05-04 23:36:18 -0400

    Form validation and reusable text input

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -7,9 +7,15 @@ import { PagingHeaderComponent } from './components/paging-header/paging-header.
 import { PagerComponent } from './components/pager/pager.component';
 import { OrderTotalsComponent } from './order-totals/order-totals.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { TextInputComponent } from './components/text-input/text-input.component';
 
 @NgModule({
-  declarations: [PagingHeaderComponent, PagerComponent, OrderTotalsComponent],
+  declarations: [
+    PagingHeaderComponent,
+    PagerComponent,
+    OrderTotalsComponent,
+    TextInputComponent,
+  ],
   imports: [
     CommonModule,
     PaginationModule.forRoot(),
@@ -25,6 +31,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     BsDropdownModule,
     OrderTotalsComponent,
     ReactiveFormsModule,
+    TextInputComponent,
   ],
 })
 export class SharedModule {}
