dfdb1eb69bb55144809c0ddffb6e896a60e5e66f
Florin Valean <36827144+fvalean@users.noreply.github.com>
2020-05-05 00:04:13 -0400

    Added the register form

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,15 +1,50 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
+  registerForm: FormGroup;
+  errors: string[];
 
-  constructor() { }
+  constructor(
+    private accountService: AccountService,
+    private fb: FormBuilder,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.createRegisterForm();
   }
 
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      displayName: [null, [Validators.required]],
+      email: [
+        null,
+        [
+          Validators.required,
+          Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$'),
+        ],
+      ],
+      password: [null, Validators.required],
+    });
+  }
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe(
+      () => {
+        this.router.navigateByUrl('/shop');
+      },
+      (error) => {
+        console.log(error);
+        this.errors = error.errors;
+      }
+    );
+  }
 }
