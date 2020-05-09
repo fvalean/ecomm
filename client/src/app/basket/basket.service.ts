3ad72c4fb227a32f576eb5f39f723987b03974a1
Florin Valean <36827144+fvalean@users.noreply.github.com>
2020-05-09 01:53:30 -0400

    Order submission

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -51,12 +51,16 @@ export class BasketService {
     );
   }
 
+  deleteLocalBasket() {
+    this.basketSource.next(null);
+    this.basketTotalSource.next(null);
+    localStorage.removeItem('basket_id');
+  }
+
   deleteBasket(basket: IBasket) {
     return this.http.delete(this.baseUrl + 'basket?id=' + basket.id).subscribe(
       () => {
-        this.basketSource.next(null);
-        this.basketTotalSource.next(null);
-        localStorage.removeItem('basket_id');
+        this.deleteLocalBasket();
       },
       (error) => {
         console.log(error);
