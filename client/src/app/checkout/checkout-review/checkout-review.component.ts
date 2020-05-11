0f620f1c6b78cafff48bb8988dccb32d77c1bf1b
Florin Valean <36827144+fvalean@users.noreply.github.com>
2020-05-11 01:08:44 -0400

    Persisting the shipping price

--- client/src/app/checkout/checkout-review/checkout-review.component.ts
+++ client/src/app/checkout/checkout-review/checkout-review.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { BasketService } from 'src/app/basket/basket.service';
 import { Observable } from 'rxjs';
 import { IBasket } from 'src/app/shared/models/basket';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-checkout-review',
@@ -11,9 +12,25 @@ import { IBasket } from 'src/app/shared/models/basket';
 export class CheckoutReviewComponent implements OnInit {
   basket$: Observable<IBasket>;
 
-  constructor(private basketService: BasketService) {}
+  constructor(
+    private basketService: BasketService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.basket$ = this.basketService.basket$;
+  }
+
+  createPaymentIntent() {
+    return this.basketService.createPaymentIntent().subscribe(
+      (response: any) => {
+        // TODO: For testing only remove later
+        this.toastr.success('Payment intent created');
+      },
+      (error) => {
+        console.log(error);
+        this.toastr.error(error.message);
+      }
+    );
   }
 }
