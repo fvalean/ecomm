9af0b94adf72a9490dc2e0ca8de5bcb6940639ed
Florin Valean <36827144+fvalean@users.noreply.github.com>
2020-05-11 00:41:45 -0400

    Updated client basket

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -25,14 +25,12 @@ export class BasketService {
 
   constructor(private http: HttpClient) {}
 
-  resetShippingPrice() {
-    this.shipping = 0;
-    this.calculateTotals();
-  }
-
   setShippingPrice(deliveryMethod: IDeliveryMethod) {
     this.shipping = deliveryMethod.price;
+    const basket = this.getCurrentBasketValue();
+    basket.deliveryMethodId = deliveryMethod.id;
     this.calculateTotals();
+    this.setBasket(basket);
   }
 
   getBasket(id: string) {
